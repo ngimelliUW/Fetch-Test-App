d3eed7b0c5ca27735856d98d7c838e7ffbf3e9be
ngimelliUW <gimelli@wisc.edu>
2023-02-18 12:46:12 -0600

    Initial Commit

---
+++ Fetch-Test-App/Fetch_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Test_AppApp.swift
+//  Fetch-Test-App
+//
+//  Created by Nicolas Gimelli on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
