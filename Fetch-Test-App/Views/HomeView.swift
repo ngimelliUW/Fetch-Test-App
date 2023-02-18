d751021d79f53c5e368b040da5d2d503546ef8f9
ngimelliUW <gimelli@wisc.edu>
2023-02-18 15:28:43 -0600

    add preliminary filter bar

--- Fetch-Test-App/Views/HomeView.swift
+++ Fetch-Test-App/Views/HomeView.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct HomeView: View {
     @StateObject var listModel: ListModel = ListModel()
-    
+    @State var selectedFilter: Int = 0 // 0 correponds to "view all", 1 corresponds to list 1, etc.
+
     var body: some View {
         VStack {
             navBar
+            filterBar
             List {
                 ForEach(listModel.listItems) { item in
                     ListItemView(item: item)
@@ -33,6 +35,37 @@ struct HomeView: View {
             Spacer()
         }
         .padding()
+    }
+
+    private var filterBar: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ZStack {
+                    Capsule()
+                        .frame(width: 80, height: 30)
+                        .foregroundColor(selectedFilter == 0 ? .primary : .gray.opacity(0.1))
+                    Text("View All")
+                        .bold()
+                        .foregroundColor(selectedFilter == 0 ? .white : .black)
+                }
+                .onTapGesture { selectedFilter = 0 }
+
+                ForEach(listModel.listIds, id: \.self) { id in
+                    ZStack {
+                        Capsule()
+                            .frame(width: 60, height: 30)
+                            .foregroundColor(selectedFilter == id ? .primary : .gray.opacity(0.1))
+                        Text("List \(id)")
+                            .foregroundColor(selectedFilter == id ? .white : .black)
+                    }
+                    .onTapGesture { selectedFilter = id }
+                }
+
+                Spacer()
+            }
+            .padding(.horizontal, 30)
+        }
+        .padding(.bottom)
     }
 }
 
