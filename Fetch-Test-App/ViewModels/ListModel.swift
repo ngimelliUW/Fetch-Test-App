0a4f11cfe1748d879ca64dd038be006d648fbf63
ngimelliUW <gimelli@wisc.edu>
2023-02-18 18:09:44 -0600

    qol

--- Fetch-Test-App/ViewModels/ListModel.swift
+++ Fetch-Test-App/ViewModels/ListModel.swift
@@ -10,26 +10,26 @@ import Foundation
 class ListModel: ObservableObject {
     @Published var listItems: [ListItem] = [ListItem]()
     var listIds: [Int] = []
-    
-    let urlStr: String = "https://fetch-hiring.s3.amazonaws.com/hiring.json"
+
+    private let urlStr: String = "https://fetch-hiring.s3.amazonaws.com/hiring.json"
 
     init() {
         fetchListItems()
     }
-    
+
     /**
      Returns a subsequence of listItems based on the given id.
-     
+
      Parameter id: The items in itemList to retrieve as a list.
      */
     func getListItemsById(id: Int) -> [ListItem] {
         if id == 0 {
             return listItems
         }
-        
-        return listItems.filter({$0.listId == id})
+
+        return listItems.filter({ $0.listId == id })
     }
-    
+
     /**
      Makes an API request to url, then calls the initializing methods for sorting and filtration
      */
@@ -51,35 +51,39 @@ class ListModel: ObservableObject {
             }.resume()
         }
     }
-    
+
     /**
      Removes items from listItems with nil or empty names
      */
     private func filterEmptyNames() {
-        self.listItems = listItems.filter({$0.name != nil && !$0.name!.isEmpty})
+        listItems = listItems.filter({ $0.name != nil && !$0.name!.isEmpty })
     }
-        
+
     /**
      Sorts list items first based on their listId then based on their name.
-     Because item names directly correspond to their id, this is a more efficient way of sorting.
+     Note:
      */
     private func sortListItems() {
-        self.listItems = listItems.sorted(by: {
+        listItems = listItems.sorted(by: {
             if $0.listId != $1.listId {
                 return $0.listId < $1.listId
             } else {
+                // strictly following the specification, one could sort by string comparison:
+                // return $0.name ?? "" < $1.name ?? ""
+
+                // sorting by item id's allows items to be "sorted by name", but have lower number items come first:
                 return $0.id < $1.id // name always corresponds to "Item \(id)", thus this line sorts by name
             }
         })
     }
-    
+
     /**
      Initializes the listIds field, which generate one filter per id in HomeView()
      */
     private func initListIds() {
-        for item in self.listItems {
-            if !self.listIds.contains(item.listId) {
-                self.listIds.append(item.listId)
+        for item in listItems {
+            if !listIds.contains(item.listId) {
+                listIds.append(item.listId)
             }
         }
     }
